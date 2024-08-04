b1c0ba13c54206dae1edb2cfcb450b32e8e42192
Jackson Mwangi <jakanphonestore@gmail.com>
2024-08-04 17:11:07 +0300

    Add script to print table description for 'Books' from 'alx_book_store' database

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+SELECT
+    COLUMN_NAME AS "Column Name",
+    COLUMN_TYPE AS "Column Type",
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS "Default Value",
+    EXTRA AS "Extra Info"
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
