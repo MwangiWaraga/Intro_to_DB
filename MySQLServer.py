e2f95fd58ece31e77ae5db3505b5101101d91952
Jackson Mwangi <jakanphonestore@gmail.com>
2024-08-04 15:31:12 +0300

    Fixed Mysql connection error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,27 +1,28 @@
 import mysql.connector
+from mysql.connector import Error
 
 try:
-    # Establish a connection to the MySQL server
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Waraga@designs6"
-    )
+    host = "localhost",
+    user = "username",
+    password = "password"
+        )
 
-    if mydb.is_connected():
-        print("Database connection opened successfully")
-
-    mycursor = mydb.cursor()
-
-    # Create the database if it does not exist
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
-
-    # Close cursor and connection inside the try block
-    mycursor.close()
-    mydb.close()
-    print("Database connection closed")
+    print("Database connection open sucessfully")
 
 except Error as e:
-    # This block catches and handles any connection errors
-    print(f"Error: {e}")
+    print(e)
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute(
+    "CREATE DATABASE IF NOT EXISTS alx_book_store"
+)
+
+print("Database 'alx_book_store' created successfully!")
+
+mycursor.close()
+mydb.close()
+
+print("Database connection closed")
